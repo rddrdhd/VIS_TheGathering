f262c9aecbf0e4e7f2fbaa29b8878b2ab2f03d74
Leona <zur0037@vsb.cz>
2019-10-23 18:30:17 +0200

    working query in index.php

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
-ahoj
+<!DOCTYPE html>
+<html>
+<body>
+here:
 <?php
   DEFINE('DB_USERNAME', 'root');
   DEFINE('DB_PASSWORD', 'root');
   DEFINE('DB_HOST', 'localhost');
   DEFINE('DB_DATABASE', 'vis');
 
-  $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+  $connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
   if (mysqli_connect_error()) {
     die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
   }
 
-  echo 'Connected successfully.';
+  echo "a";
+  $sql = "SELECT userID, rights FROM user";
+  
+  
+  echo "b";
+  $result = $connection->query($sql);
+  
+  echo "c";
+  if ($result->num_rows > 0) {
 
-  $mysqli->close();
+  echo "d";
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          echo "<br> id: ". 
+          $row["userID"]. " - Rights: ". 
+          $row["rights"]. "<br>";
+      }
+  } else {
+    echo "0 results";
+}
+
+  $connection->close();
 ?>
 
+</body>
+</html>
