38800c20bb911f66f44c4dc2803207f931ee62c5
Leona <zur0037@vsb.cz>
2019-12-10 19:09:27 +0100

    GET method

--- Views/API/new-tournament-API.php
+++ Views/API/new-tournament-API.php
@@ -1,1 +1,21 @@
 NEW-TOURNAMENT-API<?php
+# vis.l/indexAPI.php/tournaments/new?tTitle=XXX&tDate=2019-12-13T23:58&tLocation=OVA&tPrize=&tPrice=&tType=restricted&tNote=Ahoj&tAuthorID=1
+$title = $_GET['tTitle'];
+$date = $_GET['tDate'];
+$location = $_GET['tLocation'];
+$price = $_GET['tPrice'];
+$prize = $_GET['tPrize'];
+$type = $_GET['tType'];
+$note = $_GET['tNote'];
+$authorID = $_GET['tAuthorID'];
+$content =
+    "title,".$title.";".
+    "date,".$date.";".
+    "location,".$location.";".
+    "price,".$price.";".
+    "prize,".$prize.";".
+    "type,".$type.";".
+    "note,".$note.";";
+
+PostDAO::addTournament($content);
+echo "OK:<br>".$content;
