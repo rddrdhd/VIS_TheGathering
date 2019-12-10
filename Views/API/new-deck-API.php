8c1e6598e2ff58f4a8aebea06f012e038f055bec
Leona <zur0037@vsb.cz>
2019-12-10 19:15:51 +0100

    add-deck API (GET method)

--- Views/API/new-deck-API.php
+++ Views/API/new-deck-API.php
@@ -1,1 +1,16 @@
-NEW-DECK-API<?php
+<?php
+# vis.l/indexAPI.php/decks/new?deckTitle=ahoj&count=2&card1=123&card2=234
+$deckTitle = $_GET['deckTitle'];
+$count = $_GET['count'];
+if($count){
+    $deckID = DeckDAO::addDeck($deckTitle, null);
+    $cards = Array();
+    for($i = 1; $i<=$count; $i++){
+        $cardID = $_GET['card'.$i];
+        echo $i!=1?", ":"";
+        echo $cardID;
+        array_push($cards, $cardID);
+    }
+    echo "<br>celkem ".$count." karet v decku ".$deckTitle;
+    DeckDAO::addCards($cards, $deckID);
+}
